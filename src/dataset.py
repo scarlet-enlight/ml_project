c9c2e2c6171dec0bfec65d7b002017afa6e15cde
blaze <blazejostaszewski@gmail.com>
2025-05-25 20:29:19 +0200

    create a test of knn

--- src/dataset.py
+++ src/dataset.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-
 from loguru import logger
 from tqdm import tqdm
 import typer
+import pandas as pd
 
 from src.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
@@ -11,18 +11,18 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
+    input_path: Path = RAW_DATA_DIR / "tested.csv",
+    output_path: Path = PROCESSED_DATA_DIR / "tested.csv",
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+    df = pd.read_csv(input_path)
+    df = df.drop(columns = ['Ticket', 'Name','Cabin', 'Embarked'])
+    df['Sex'] = df['Sex'].map({'female':0, 'male': 1})
+    df.fillna(df.mean(), inplace=True)
+    df.to_csv(output_path, index=False)
+    df.describe()
+    df.info()
+    
+
 
 
 if __name__ == "__main__":
--- src/features.py
+++ src/features.py
@@ -3,6 +3,10 @@ from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 import typer
+import pandas as pd
+from models.knn_model import knn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 
 from src.config import PROCESSED_DATA_DIR
 
@@ -12,17 +16,23 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+    input_path: Path = PROCESSED_DATA_DIR / "tested.csv",
     output_path: Path = PROCESSED_DATA_DIR / "features.csv",
     # -----------------------------------------
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Features generation complete.")
-    # -----------------------------------------
+    #kod knn
+    df = pd.read_csv(input_path)
+    X = df.drop(columns = 'Survived')
+    y = df['Survived']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.4)
+    print(X_train)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    print(accuracy_score(y_test, y_pred))
+    print(classification_report(y_test, y_pred))
+    
+    
+
 
 
 if __name__ == "__main__":
